5147ded54e93ee7258bfa06542e587bdbf7fd9e4
Stephen Hardisty <shardisty@etsy.com>
2011-08-31 14:12:17 -0400

    Added wildcard and directory matching for command line score script

--- scripts/despamilator_score.rb
+++ scripts/despamilator_score.rb
@@ -3,23 +3,29 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'despamilator')
 require 'zlib'
 
-file = ARGV[0] || raise("Usage: despamilator_spec.rb [filename]")
+def check_file file
+  text = file =~ /\.gz$/i ? Zlib::GzipReader.open(file).read : File.open(file).read
 
-text = file =~ /\.gz$/i ? Zlib::GzipReader.open(file).read : File.open(file).read
-
-puts "Testing:"
-puts "========================"
-puts text
-puts "========================"
-puts "\n"
-
-dspam = Despamilator.new(text)
-
-puts "Total Score: #{dspam.score}\n\n"
-
-puts "Matched by..." unless dspam.matches.empty?
-dspam.matches.each do |match|
-  puts "\tFilter: #{match[:filter].name}"
-  puts "\tScore:  #{match[:score]}"
+  puts "Testing #{file}:"
+  puts "========================"
+  puts text
+  puts "========================"
   puts "\n"
+
+  dspam = Despamilator.new(text)
+
+  puts "Total Score: #{dspam.score}\n\n"
+
+  puts "Matched by..." unless dspam.matches.empty?
+  dspam.matches.each do |match|
+    puts "\tFilter: #{match[:filter].name}"
+    puts "\tScore:  #{match[:score]}"
+    puts "\n"
+  end
+end
+
+file_or_dir = ARGV[0] || raise("Usage: despamilator_spec.rb [file or dir]")
+
+Dir.glob(file_or_dir).each do |file|
+  check_file file
 end
