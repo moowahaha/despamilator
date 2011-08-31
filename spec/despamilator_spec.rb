c4ec5adf18fff6f8356c48c08f683b3cd4065a96
Stephen Hardisty <shardisty@etsy.com>
2011-08-31 14:14:04 -0400

    Put coverage over dead method

--- spec/despamilator_spec.rb
+++ spec/despamilator_spec.rb
@@ -3,11 +3,15 @@ describe Despamilator do
     @dspam = Despamilator.new('this text is absolutely fine')
   end
 
-  it "should return a zero score for fine text" do
+  it 'should return a zero score for fine text' do
     @dspam.score.should == 0
   end
 
-  it "should return no matching filter for fine text" do
+  it 'should return no matching filter for fine text' do
     @dspam.matches.should be_empty
+  end
+
+  it 'should raise and exception when we call a dead method' do
+    -> {@dspam.matched_by}.should raise_error
   end
 end
